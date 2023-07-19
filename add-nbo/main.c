6c83e0e5b36105ca8b2e77096a9197ad26ee5ff1
gleaming0 <aheun1216@naver.com>
2023-07-19 23:16:00 +0900

    Upload add-nbo

--- add-nbo/main.c
+++ add-nbo/main.c
@@ -1,7 +1,8 @@
-// main.cpp
+// main.c
 #include <stdio.h>
 #include <stdlib.h>
 #include <stdint.h>
+
 
 uint32_t my_ntol(uint32_t a) {
         uint32_t a1 = (a & 0x000000ff) << 24;
@@ -9,8 +10,10 @@ uint32_t my_ntol(uint32_t a) {
         uint32_t a3 = (a & 0x00ff0000) >> 8;
         uint32_t a4 = (a & 0xff000000) >> 24;
         a = a1 | a2 | a3 | a4;
+
         return a;
 }
+
 
 uint32_t readFile(char* filename)
 {
@@ -18,6 +21,7 @@ uint32_t readFile(char* filename)
 
 	uint32_t value = 0;
 	uint32_t nvalue = 0;
+
 	fread(&value, sizeof(uint32_t), 1, file);
 	fclose(file);
 
@@ -25,26 +29,46 @@ uint32_t readFile(char* filename)
 	return nvalue;
 }
 
-void printValue(unsigned char* buffer, long int size) {
-	size_t idx = 0;
 
-	// 16진수를 10진수로 변환하여 출력
-        unsigned int val1_d = 0;
-        for (size_t i = idx; i < size; ++i) {
-                val1_d = val1_d * 256 + buffer[i];
-        }
-        printf("%d(0x", val1_d);
+unsigned int hextodec(uint32_t value) {
+	unsigned int decvalue = 0;
+	unsigned int base = 1;
 
-	// 앞에 있는 0을 찾기
-	while (idx < size && buffer[idx] == 0) {
-		idx++;
+	while (value > 0) {
+		unsigned int digit = value & 0xF;
+		decvalue += digit * base;
+		base *= 16;
+		value >>= 4;
 	}
 
-	// 0을 제외한 나머지 데이터 출력
-	for (size_t i = idx; i < size; ++i) {
-		printf("%02X", buffer[i]);
-	}
-	printf(")\n");
+	return decvalue;
+}
+
+
+uint32_t sum(uint32_t a, uint32_t b)
+{
+	uint32_t result = 0;
+	uint16_t a1 = (a & 0xff00) >> 8;
+	uint16_t a2 = (a & 0x00ff) << 8 ;
+	uint16_t a3 = (a1 | a2);
+	uint16_t b1 = (b & 0xff00) >> 8;
+	uint16_t b2 = (b & 0x00ff) << 8 ;
+	uint16_t b3 = (b1 | b2);
+
+	return a3 | b3;
+
+
+}
+
+
+uint16_t my_ntohs(uint16_t a) {
+        uint16_t a1 = (a & 0xff00) >> 8;        //3400 -> 0034
+        printf("a1 = 0x%x\n", a1);
+        uint16_t a2 = (a & 0x00ff) << 8 ;       //0012 -> 1200
+        printf("a2 = 0x%x\n", a2);
+        uint16_t a3 = (a1 | a2);                //0034 | 1200
+        printf("a3 = 0x%x\n", a3);
+        return a3;
 }
 
 
@@ -55,13 +79,18 @@ int main(int argc, char* argv[])
 		return 1;
     	}
 
+	uint32_t hex1, hex2, hex3;
+	unsigned int dec1, dec2, dec3;
+
 	char* filename1 = argv[1];
 	char* filename2 = argv[2];
 
-	uint32_t val1 = readFile(filename1);
-	uint32_t val2 = readFile(filename2);
+	hex1 = readFile(filename1);
+	hex2 = readFile(filename2);
+	dec1 = hextodec(hex1);
+	dec2 = hextodec(hex2);
 
-	printf("0x%08X, 0x%08X\n", val1, val2);
+	printf("%d(0x%X) + %d(0x%X) = %d(0x%X)\n", dec1, hex1, dec2, hex2, dec1+dec2, hex1+hex2);
 
 	return 0;
 }
